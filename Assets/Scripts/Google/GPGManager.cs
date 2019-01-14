53484be800f1c3383e222dc2e5816edd358ae690
Jai Sharma <jai.prakash@splashmath.com>
2019-01-14 12:32:48 +0530

    GPG added

---
+++ Assets/Scripts/Google/GPGManager.cs
@@ -0,0 +1,33 @@
+﻿using GooglePlayGames;
+using UnityEngine.SocialPlatforms;
+using UnityEngine;
+
+public class GPGManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success)
+            {
+                OnLoginSuccess();
+            }
+            else
+            {
+                OnLoginFailure();
+            }
+        });
+    }
+
+    private void OnLoginSuccess()
+    {
+
+    }
+
+    private void OnLoginFailure()
+    {
+
+    }
+}
